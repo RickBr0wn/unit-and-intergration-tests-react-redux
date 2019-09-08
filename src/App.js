0249802771928ea90777ed8a3553c1745e42464c
Rick Brown <ricky.brown.00@gmail.com>
2019-09-08 11:06:44 +0100

    add headline component and test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import Headline from './components/Headline'
 
 function App() {
   return (
     <div>
       <Header />
+      <section className='main'>
+        <Headline header='Posts' desc='Click the button to load the posts!' />
+      </section>
     </div>
   )
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,10 @@
 export const findByTestAttr = (component, attr) =>
   component.find(`[test-attr='${attr}']`)
+
+export const CONSTANTS = {
+  TEST_STR: 'TEST_STR',
+  TEST_NUM: 42,
+  TEST_BOOL: true,
+  TEST_OBJ: {},
+  TEST_FUNC: () => {}
+}
